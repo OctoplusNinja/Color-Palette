22024eabd60362498cea98815e45917bfc20f0a4
Aniruddha Das <aniruddha.das.852@gmail.com>
2022-07-14 18:15:38 +0530

    Initial Palette

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Palette from "./components/Palette/Palette";
+import seedColors from "./seedColors";
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[8]} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Palette/Palette.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ColorBox from "../ColorBox/ColorBox";
+import "./Palette.css";
+
+class Palette extends Component {
+  render() {
+    const colorBoxes = this.props.colors.map((color) => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
+    return (
+      <div className="Palette">
+        {/* Navbar Goes Here */}
+        <div className="Palette-colors">{colorBoxes}</div>
+        {/* Footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
