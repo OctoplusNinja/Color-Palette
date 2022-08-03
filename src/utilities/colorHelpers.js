8f219d613d9c573ed45f7615dd2c45b11920a7e9
Aniruddha Das <aniruddha.das.852@gmail.com>
2022-08-03 14:56:45 +0530

    Added code to generate different shades of colors

---
+++ src/utilities/colorHelpers.js
@@ -0,0 +1,44 @@
+import chroma, { hex } from "chroma-js";
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+function generatePalette(starterPalette) {
+    let newPalette = {
+        palette: starterPalette.paletteName,
+        id: starterPalette.id,
+        emoji: starterPalette.emoji,
+        colors: {}
+    }
+    for (let level of levels) {
+        newPalette.colors[level] = []
+    }
+    for (let color of starterPalette.colors) {
+        let scale = generateScale(color.color, 10);
+        for (let i in scale) {
+            newPalette.colors[levels[i]].push(
+                {
+                    name: `${color.name} ${levels[i]}`,
+                    id: color.name.toLowerCase().replace(/ /g, "-"),
+                    hex: scale[i],
+                    rgb: chroma(scale[i]).css(),
+                    rgba: chroma(scale[i]).css().replace("rgb", "rgba").replace(")", ",1.0)")
+                }
+            )
+        }
+    }
+    return newPalette;
+}
+
+function getRange(hexColor) {
+    const start = "#fff"
+    return [
+        start,
+        hexColor,
+        chroma(hexColor).darken(1.4).hex()
+    ]
+}
+
+function generateScale(hexColor, numberOfColors) {
+    return chroma.scale(getRange(hexColor)).mode("lab").colors(numberOfColors)
+}
+
+export { generatePalette };
