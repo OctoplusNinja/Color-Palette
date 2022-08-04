680fb6359e821c89c43695b9e32d5e92ee3bbcf8
Aniruddha Das <aniruddha.das.852@gmail.com>
2022-08-05 00:56:28 +0530

    Added Copy Mode

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import "rc-slider/assets/index.css";
-import Slider from "rc-slider";
+import Slider from "@mui/material/Slider";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import "./NavBar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="NavBar">
         <div className="logo">
@@ -19,9 +30,18 @@ class Navbar extends Component {
               min={100}
               max={900}
               step={100}
+              color="secondary"
+              marks
               onChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select onChange={this.handleChange} value={format}>
+            <MenuItem value="hex">HEX - #FFFFFF</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(10,20,30)</MenuItem>
+            <MenuItem value="rgba">RGBA = rgba(10,20,30,1)</MenuItem>
+          </Select>
         </div>
       </header>
     );
